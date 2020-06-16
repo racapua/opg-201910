8e868fd6528ebe435ea7ab18282f0a0199283f58
Richard Anderson Capua <ra.capua@gmail.com>
2020-06-17 02:39:36 +0800

    added tests

--- opg-201910-interview/Models/Client.cs
+++ opg-201910-interview/Models/Client.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 namespace opg_201910_interview.Models
 {
     public class Client
     {
-      
+        public List<string> FileOrder;
+        public string ClientID;
+        public string ClientName;
+
+        public List<string> Arrange(List<string> files) {
+            return new List<string>{};
+        }
+
+        private DateFormatType GetDateFormatType(List<string> files) {
+            return DateFormatType.NoSpaces;
+        }
+
+    }
+
+    public enum DateFormatType {
+        Hyphens,
+        NoSpaces
     }
 }
