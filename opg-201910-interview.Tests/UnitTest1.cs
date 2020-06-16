7b7ea56033439ab54adb018120eae3453dc4afda
Richard Anderson Capua <ra.capua@gmail.com>
2020-06-16 17:18:12 +0800

    project rearrangement

---
+++ opg-201910-interview.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace opg_201910_interview.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ opg-201910-interview/Services/UploadService.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace opg_201910_interview.Services
+{
+    public class UploadService
+    {
+      
+    }
+}
